7d36c3a220e986dbcfbb83244eaab8e5f199a1ca
Jonatas Olziris Martins <Jonatas.Martins@highjump.com>
2022-10-20 19:19:54 -0300

    Adding more code examples.

--- Jala-university-Aula8/Program.cs
+++ Jala-university-Aula8/Program.cs
@@ -2,17 +2,26 @@
 
 
 var minhaclasse = new MinhaClasse();
+
 var numero = 56;
 Console.WriteLine(numero);
-Console.WriteLine(minhaclasse.NumeroMaiorQueZero(numero));
-
+Console.WriteLine(minhaclasse.ValidarSeNumeroMaiorQueZero(numero));
 Console.WriteLine(numero);
+
+var outroNumero = 22;
+Console.WriteLine(outroNumero);
+Console.WriteLine(minhaclasse.PassandoParameterPorReferencia(ref outroNumero));
+Console.WriteLine(outroNumero);
 
 public class MinhaClasse
 {
-    public string Nome { get; set; }
-
-    public bool NumeroMaiorQueZero(int numero)
+    public bool ValidarSeNumeroMaiorQueZero(int numero)
+    {
+        numero = 0;
+        return numero > 0;
+    }
+    
+    public bool PassandoParameterPorReferencia(ref int numero)
     {
         numero = 0;
         return numero > 0;
